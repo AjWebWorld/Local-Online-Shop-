6897b70bb3dbacebb2b37ba750fea6681eb6b5b7
AjWebWorld <126265266+AjWebWorld@users.noreply.github.com>
2023-03-18 16:51:21 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,10 @@
+// form Loading animation
+
+const form = [...document.querySelector('.form').children];
+
+form.forEach((item, i) => {
+    setTimeout(() => {
+        item.style.opacity = 1;
+
+    }, i*100)
+} )
